785f6dc277c7257857a72d84c0c7b7b0459480c3
gogetta69 <79624312+gogetta69@users.noreply.github.com>
2024-07-30 10:59:11 -0600

    Add files via upload

--- xmltv.php
+++ xmltv.php
@@ -6,18 +6,48 @@ ini_set('memory_limit', '512M');
 $epgUrls = [
     "http://m3u4u.com/xml/jwmzn1wx72cj6m8dn721",   
     "https://raw.githubusercontent.com/matthuisman/i.mjh.nz/master/PlutoTV/us.xml",
-    "https://epg.pw/xmltv/epg_ZA.xml"
-    // "channels/thetvapp_sports_epg.xml" // Disabled until a better sync method is found.
+    "https://epg.pw/xmltv/epg_ZA.xml",
+    "https://epg.pw/api/epg.xml?channel_id=9025",
+    "https://epg.pw/api/epg.xml?channel_id=8862",
+    "https://epg.pw/api/epg.xml?channel_id=8306"
 ];
 
 $mergedEpgFile = "channels/epg.xml";
 $lastUpdatedFile = "channels/last_updated_epg.txt";
 
+function fetchEPGContent($url) {
+    $context = stream_context_create(array(
+        'http' => array(
+            'timeout' => 30
+        )
+    ));
+    $content = @file_get_contents($url, false, $context);
+    if ($content === false) {
+        error_log("Failed to fetch URL: $url");
+        return false;
+    }
+    return $content;
+}
+
+function fixXMLIssues($xmlContent) {
+    // Fix common encoding issues
+    $xmlContent = str_replace('&amp;amp;', '&amp;', $xmlContent);
+    $xmlContent = str_replace('&', '&amp;', $xmlContent);
+    
+    // Ensure each <programme> tag is properly closed and separated
+    $xmlContent = preg_replace('/<\/programme>\s*<programme/', "</programme>\n<programme", $xmlContent);
+    
+    // Clean up any hidden or special characters
+    $xmlContent = preg_replace('/[^\x20-\x7E]/', '', $xmlContent);
+    
+    return $xmlContent;
+}
+
 if (!file_exists($lastUpdatedFile) || (time() - file_get_contents($lastUpdatedFile)) > 10800) {
     $mergedXml = new SimpleXMLElement('<tv/>');
 
     foreach ($epgUrls as $url) {
-        $epgContent = @file_get_contents($url);
+        $epgContent = fetchEPGContent($url);
        
         if ($epgContent === false) {
             continue;
@@ -27,11 +57,25 @@ if (!file_exists($lastUpdatedFile) || (time() - file_get_contents($lastUpdatedFi
         if (substr($url, -3) == '.gz') {
             $epgContent = @gzdecode($epgContent);
             if ($epgContent === false) {
+                error_log("Failed to decompress gzipped content from URL: $url");
                 continue;
             }
         }
 
-        $xml = new SimpleXMLElement($epgContent);
+        // Fix the XML issues before parsing
+        $epgContent = fixXMLIssues($epgContent);
+
+        // Check if the XML content is valid
+        if (!preg_match('/<tv[^>]*>.*<\/tv>/s', $epgContent)) {
+            error_log("Invalid XML structure from URL: $url");
+            continue;
+        }
+
+        $xml = @simplexml_load_string($epgContent);
+        if ($xml === false) {
+            error_log("Failed to parse XML from URL: $url");
+            continue;
+        }
 
         // Merge the data
         foreach ($xml->channel as $channel) {
@@ -53,7 +97,5 @@ if (!file_exists($lastUpdatedFile) || (time() - file_get_contents($lastUpdatedFi
 }
 
 echo @file_get_contents($mergedEpgFile);
-
-
 
 ?>
